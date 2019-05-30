b11b9cb7af93f2ac871c43ecf7b5cf2923bbd569
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2019-05-30 14:47:24 +0200

    Build tailwind and include in built output

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { AIRLINES } from './airlines';
+import './index.css';
 
 ReactDOM.render(<App airlines={AIRLINES} />, document.getElementById('root'));
 
