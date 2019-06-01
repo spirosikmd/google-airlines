33c13cf364ac5a8fe26b7d98728b9bd46ce3a7fb
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2019-06-01 11:18:11 +0200

    Purge tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.tsx', './public/index.html'],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
+};
